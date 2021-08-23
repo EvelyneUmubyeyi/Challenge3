da5ebf55a02ca0a0ad6eda35b2a801b57cd23ecb
EvelyneUmubyeyi <umubyeyievelyne8917@gmail.com>
2021-08-23 22:46:13 +0200

    responsiveness

---
+++ js.js
@@ -0,0 +1,24 @@
+function emailContainAlphabetsSpecialChars() { 
+    var checkemail=document.getElementById("email").value;
+
+    if (checkemail!="") { 
+
+            var emailValidationRegularExpression = /\S+@\S+\.\S+/;
+
+            var validation = emailValidationRegularExpression.test(document.getElementById("email").value);
+
+            if (!validation) {
+                document.getElementById("email").style.borderColor='red';
+                document.getElementById("error").innerHTML="Check your email please";
+            } else { 
+                document.getElementById("success").innerHTML="Email submitted";
+                document.getElementById("error").style.display='none';
+                document.getElementById("email").style.borderColor='green';
+            }
+            return validation;
+   }else{
+    document.getElementById("email").style.borderColor='red';
+    document.getElementById("error").innerHTML="Whoops! It looks like you forgot to add your email";
+     
+   } 
+}
